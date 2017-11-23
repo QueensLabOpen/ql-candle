4955320db38b095015af969d80089d3e34793832
Ben <benh.schneider@gmail.com>
2017-11-23 20:29:07 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	frontend/js/main.js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -8,14 +8,14 @@ var handler = {
 	},
 	
 	processEvent: function (event) {
-		// console.log(event);
-		// console.log(this.imageDictionary[2]);
-		// //$('#message').html(event.alpha+' '+event.beta+' '+event.gamma)
-		var tiltX =  Math.round(event.beta/90 % 2); //Math.abs( Math.round( event.beta / 90 + 1) * 2 )
-		// var tiltY =  Math.round( event.gamma / 180 + 1) * 2;
-		$('#message').html(tiltX);
-		// $('#candle-img').attr('src',this.imageDictionary[0]);
-		// $('#message').html(tiltX+' '+tiltY)
+		console.log(event);
+		console.log(this.imageDictionary[2]);
+		//$('#message').html(event.alpha+' '+event.beta+' '+event.gamma)
+		var tiltX =  Math.round( event.beta / 90 ) ;
+		var tiltY =  Math.round( event.gamma );
+		$('#message').html(this.imageDictionary[tiltX]);
+		$('#candle-img').attr('src',this.imageDictionary[tiltX + 2]);
+		$('#message').html(tiltX+' '+tiltY)
 	}
 };
 
