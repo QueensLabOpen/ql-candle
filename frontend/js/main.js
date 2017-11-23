196ad3582b5b79288c592a8fc76d53066b8dea93
Marcus Nordquist <marcus.nordquist@keenear.se>
2017-11-23 19:20:59 +0100

    no message

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,17 +2,17 @@
 
 if (window.DeviceOrientationEvent) {
     window.addEventListener("deviceorientation", function () {
-		$('#message').replace(event.alpha+' '+event.beta+' '+event.gamma);
+		$('#message').html(event.alpha+' '+event.beta+' '+event.gamma);
         //tilt([event.beta, event.gamma]);
     }, true);
 } else if (window.DeviceMotionEvent) {
     window.addEventListener('devicemotion', function () {
-		$('#message').replace(event.alpha+' '+event.beta+' '+event.gamma);
+		$('#message').html(event.alpha+' '+event.beta+' '+event.gamma);
         //tilt([event.acceleration.x * 2, event.acceleration.y * 2]);
     }, true);
 } else {
     window.addEventListener("MozOrientation", function () {
-		$('#message').replace(event.alpha+' '+event.beta+' '+event.gamma);
+		$('#message').html(event.alpha+' '+event.beta+' '+event.gamma);
         //tilt([orientation.x * 50, orientation.y * 50]);
     }, true);
 }
