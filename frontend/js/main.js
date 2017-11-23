0c42bf27e1e124d83fc2bb1bfb848aefa385398a
Marcus Nordquist <marcus.nordquist@keenear.se>
2017-11-23 21:06:50 +0100

    sdfadsfdfas

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,8 +3,8 @@ var handler = {
 	imageDictionary: {
 		0:"img/candleoff.gif", 		
 		1:"img/candleoff.gif", 
-		2:"img/candle-anim.gif",
-		3:"img/candleoff.gif",
+		2:"img/candleoff.gif",
+		3:"img/candle-anim.gif",
 		4:"img/candleoff.gif"
 	},
 	
@@ -13,9 +13,9 @@ var handler = {
 		console.log(this.imageDictionary[2]);
 		var tiltX =  Math.round( event.beta / 90 ) + 2; 
 		//var tiltY =  Math.round( event.gamma );
-		$('#message').html(this.imageDictionary[tiltX]);
+		//$('#message').html(this.imageDictionary[tiltX]);
 		$('#candle-img').attr('src',this.imageDictionary[tiltX]);
-		$('#message').html(tiltX+' '+tiltY)
+		$('#message').html('TiltX: '+tiltX+' TiltY: '+tiltY+' Image: '+this.imageDictionary[tiltX])
 	}
 };
 
