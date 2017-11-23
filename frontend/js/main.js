b28f41a6d6f2f6c299622ebedf478b3835731227
Marcus Nordquist <marcus.nordquist@keenear.se>
2017-11-23 20:26:29 +0100

    flkjdsf

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,18 +3,19 @@ var handler = {
 	imageDictionary: {
 		0:"img/candleoff.gif", 		
 		1:"img/candleon.gif", 
-		2:"img/candleoff.gif"
+		2:"img/candleanim.gif",
+		3:"img/candleanim.gif"
 	},
 	
 	processEvent: function (event) {
-		console.log(event);
-		console.log(this.imageDictionary[2]);
-		//$('#message').html(event.alpha+' '+event.beta+' '+event.gamma)
+		// console.log(event);
+		// console.log(this.imageDictionary[2]);
+		// //$('#message').html(event.alpha+' '+event.beta+' '+event.gamma)
 		var tiltX =  Math.round(event.beta/90 % 2); //Math.abs( Math.round( event.beta / 90 + 1) * 2 )
-		var tiltY =  Math.round( event.gamma / 180 + 1) * 2;
-		$('#message').html(this.imageDictionary[tiltX]);
-		$('#candle-img').attr('src',this.imageDictionary[tiltX]);
-		//$('#message').html(tiltX+' '+tiltY)
+		// var tiltY =  Math.round( event.gamma / 180 + 1) * 2;
+		$('#message').html(tiltX);
+		// $('#candle-img').attr('src',this.imageDictionary[0]);
+		// $('#message').html(tiltX+' '+tiltY)
 	}
 };
 
