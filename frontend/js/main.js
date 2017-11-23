58885fb68fe5e181c547ff2ad0b15fbdfb0615a8
Marcus Nordquist <marcus.nordquist@keenear.se>
2017-11-23 19:46:12 +0100

    kjdfghisfd

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,16 +1,35 @@
+var handler = {
+	
+	imageDictionary: {
+		1:"img/candleon.gif", 
+		2:"img/candleoff.gif"
+	},
+	
+	processEvent: function (event) {
+		console.log(event);
+		console.log(this.imageDictionary[2]);
+		//$('#message').html(event.alpha+' '+event.beta+' '+event.gamma)
+		var tiltX =  Math.round(event.beta % 2); //Math.abs( Math.round( event.beta / 90 + 1) * 2 )
+		var tiltY =  Math.round( event.gamma / 180 + 1) * 2
+		$('#candle-img').attr('src',this.imageDictionary[tiltX]);
+		$('#message').html(tiltX+' '+tiltY)
+	}
+};
+
+
 (function() {
 
 if (window.DeviceOrientationEvent) {
     window.addEventListener("deviceorientation", function () {
-		$('#message').html(event.alpha+' '+event.beta+' '+event.gamma);
+		handler.processEvent(event);
     }, true);
 } else if (window.DeviceMotionEvent) {
     window.addEventListener('devicemotion', function () {
-		$('#message').html(event.alpha+' '+event.beta+' '+event.gamma);
+		handler.processEvent(event);
     }, true);
 } else {
     window.addEventListener("MozOrientation", function () {
-		$('#message').html(event.alpha+' '+event.beta+' '+event.gamma);
+		handler.processEvent(event);
     }, true);
 }
 
