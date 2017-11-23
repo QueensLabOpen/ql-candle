348cd0ed058044392a8b358992b729efecd165cd
Marcus Nordquist <marcus.nordquist@keenear.se>
2017-11-23 20:15:00 +0100

    anim

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,20 +1,22 @@
 var handler = {
 	
 	imageDictionary: {
-		0:"img/candleoff.gif", 		
-		1:"img/candleon.gif", 
-		2:"img/candleon.gif"
+		0:"img/candleoff.gif",
+		1:"img/candleoff.gif", 		
+		2:"img/candle-anim.gif", 
+		3:"img/candle-anim.gif"
 	},
 	
 	processEvent: function (event) {
 		console.log(event);
 		console.log(this.imageDictionary[2]);
 		//$('#message').html(event.alpha+' '+event.beta+' '+event.gamma)
-		var tiltX =  Math.round( event.beta );
-		var tiltY =  Math.round( event.gamma );
-		$('#message').html(this.imageDictionary[tiltX]);
-		$('#candle-img').attr('src',this.imageDictionary[ Math.round(tiltX/90 % 2)]);
-		$('#message').html(tiltX+' '+tiltY)
+		var tiltX =  Math.round( event.beta ) + 1;
+		var tiltXPlusStuff = Math.round(tiltX/90 % 2);
+		//var tiltY =  Math.round( event.gamma );
+		$('#message').html('TiltX: '+ tiltXtiltXPlusStuff + ' Plus: '+ tiltXPlusStuff);
+		$('#candle-img').attr('src',this.imageDictionary[ tiltXPlusStuff ]);
+		//$('#message').html(tiltX+' '+tiltY)
 	}
 };
 
