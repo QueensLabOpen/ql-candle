60f79dbc7cfeaff1205e8b913b5d56b79815adff
Marcus Nordquist <marcus.nordquist@keenear.se>
2017-11-23 18:38:17 +0100

    style, scripts

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,5 +1,20 @@
 (function() {
 
-
+if (window.DeviceOrientationEvent) {
+    window.addEventListener("deviceorientation", function () {
+		console.log(event);
+        //tilt([event.beta, event.gamma]);
+    }, true);
+} else if (window.DeviceMotionEvent) {
+    window.addEventListener('devicemotion', function () {
+		console.log(event);
+        //tilt([event.acceleration.x * 2, event.acceleration.y * 2]);
+    }, true);
+} else {
+    window.addEventListener("MozOrientation", function () {
+		console.log(event);
+        //tilt([orientation.x * 50, orientation.y * 50]);
+    }, true);
+}
 
 })();
